d80161224fc63942135098c2c4198261934d67a3
phonghoang@gmail.com <phonghoang@gmail.com>
2015-09-20 18:18:39 -0700

    finish project

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,16 +1,30 @@
 class RecordsController < ApplicationController
   def index
-  	@records = Record.all
+    # get all the records
+    @records = Record.all
   end
 
   def show
-  	@record = Record.find(params[:id])
+    # find a specific record
+    @record = Record.find(params[:id])
   end
 
   def new
-  	@record = Record.new
+    # new instance of a record
+    @record = Record.new
   end
 
   def create
+    # creating a record in the database
+    Record.create(record_params)
+    redirect_to('/records')
   end
+
+  private
+
+  # strong parameters for a record
+  def record_params
+    params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
+  
 end
